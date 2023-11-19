96d543819c51bcbb4e5f9932b5bcb4a9b5006a3f
qwwqe <f.werth@gmail.com>
2023-11-19 15:14:51 +0800

    Parse rating and ripped as integers as bools

--- ch03.lisp
+++ ch03.lisp
@@ -21,5 +21,5 @@
   (make-cd
    (prompt-read "Title")
    (prompt-read "Artist")
-   (prompt-read "Rating")
-   (prompt-read "Ripped [y/n]")))
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped: ")))
