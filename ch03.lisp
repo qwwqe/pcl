79f768004ea75d6b4cae46292c32bdfb5d0a947a
qwwqe <f.werth@gmail.com>
2023-11-19 22:01:18 +0800

    Implement select and where

--- ch03.lisp
+++ ch03.lisp
@@ -40,3 +40,14 @@
                       :direction :input)
     (with-standard-io-syntax
       (setf *db* (read in)))))
+
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title (equal (getf cd :title) title) t)
+       (if artist (equal (getf cd :artist) artist) t)
+       (if rating (equal (getf cd :rating) rating) t)
+       (if ripped-p (equal (getf cd :ripped) ripped) t))))
